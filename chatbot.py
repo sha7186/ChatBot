8eba8324a5e98734c0f62bc5ded5cd7cbb9cb6ab
Shalini <sshalini7186@gmail.com>
2023-12-15 03:42:37 +0530

    added new files

--- chatbot.py
+++ chatbot.py
@@ -8,7 +8,7 @@ from nltk.stem import WordNetLemmatizer
 from keras.models import load_model
 
 lemmatizer = WordNetLemmatizer()
-intents = json.loads(open('C:/Users/user/Desktop/Project-3/venv/intents.json').read())
+intents = json.loads(open('C:/Users/user/Desktop/Project-3/intents.json').read())
 
 # intents = json.loads(open('C:\Users\user\Desktop\Project-3\venv\').read())
 
